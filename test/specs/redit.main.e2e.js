3ec063826fef20d52564c74affd3ba27e5eb1458
Андрей Никифоров <anikiforov@web100-khm.local>
2021-04-22 15:57:37 +0300

    Commit a stable part of test.

--- test/specs/redit.main.e2e.js
+++ test/specs/redit.main.e2e.js
@@ -4,43 +4,29 @@ const RedditListPostsPage = require('../pageobjects/reddit.list.posts.page');
 const RedditPostPage = require('../pageobjects/reddit.post.page');
 
 describe('Reddit main page', () => {
-  it('Selected hot option', () => {
-    RedditMainPage.open();
-    RedditMainPage.hotPostsAction();
+  // it('Selected hot option', () => {
+  //   RedditMainPage.open();
+  //   RedditMainPage.hotPostsAction();
 
-    expect(RedditMainPage.linkTopsGrowingCommunities).toBeDisplayed();
-    expect(RedditMainPage.filterCountry).toHaveTextContaining('Everywhere');
-  });
-
-  it('Selected top option', () => {
-    RedditMainPage.open();
-    RedditMainPage.topPostsAction();
-  });
-
-  it('Change post view', () => {
-    RedditListPostsPage.open();
-
-    RedditListPostsPage.topPageCheckTitle();
-    RedditListPostsPage.topPageCheckDate();
-    RedditListPostsPage.topPageCommentsCount();
-  });
-
-  // it('Post actions', () => {
-  //   browser.addLocatorStrategy('jsQuery', () => {
-  //     const xPathResult = document.evaluate('//a[contains(@id, "CommentTopMeta--Created")]', document.body, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
-  //     const nodes = [];
-  //     let node = xPathResult.iterateNext();
-
-  //     while (node) {
-  //       nodes.push(node);
-  //       node = xPathResult.iterateNext();
-  //     }
-
-  //     return (nodes.length === 0) ? 'GG' : nodes;
-  //   });
-
-  //   RedditPostPage.open();
-  //   RedditPostPage.postActions();
-  //   browser.pause(1000000);
+  //   expect(RedditMainPage.linkTopsGrowingCommunities).toBeDisplayed();
+  //   expect(RedditMainPage.filterCountry).toHaveTextContaining('Everywhere');
   // });
+
+  // it('Selected top option', () => {
+  //   RedditMainPage.open();
+  //   RedditMainPage.topPostsAction();
+  // });
+
+  // it('Change post view', () => {
+  //   RedditListPostsPage.open();
+
+  //   RedditListPostsPage.topPageCheckTitle();
+  //   RedditListPostsPage.topPageCheckDate();
+  //   RedditListPostsPage.topPageCommentsCount();
+  // });
+
+  it('Post actions', () => {
+    RedditPostPage.open();
+    RedditPostPage.postActions();
+  });
 });

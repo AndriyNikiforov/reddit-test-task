00d2ba6e51167af4b5a75fc3fa8333e01ce9a61e
Андрей Никифоров <anikiforov@web100-khm.local>
2021-04-23 11:54:16 +0300

    Created new test spec.

---
+++ test/specs/redit.post.e2e.js
@@ -0,0 +1,19 @@
+/* eslint-disable no-undef */
+const RedditListPostsPage = require('../pageobjects/reddit.list.posts.page');
+const RedditPostPage = require('../pageobjects/reddit.post.page');
+
+describe('Reddit posts page', () => {
+  it('Change post list view', () => {
+    RedditListPostsPage.open();
+
+    RedditListPostsPage.topPageCheckTitle();
+    RedditListPostsPage.topPageCheckDate();
+    RedditListPostsPage.topPageCommentsCount();
+  });
+
+  it('Post page actions', () => {
+    RedditPostPage.open();
+    RedditPostPage.postActions();
+    RedditPostPage.openReplayModalWindow();
+  });
+});

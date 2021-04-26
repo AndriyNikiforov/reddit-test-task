0ef7f6b4b0fb3bcb9b21d23030952e5674a000fd
Андрей Никифоров <anikiforov@web100-khm.local>
2021-04-26 12:19:43 +0300

    Return part of test.

--- test/specs/redit.post.e2e.js
+++ test/specs/redit.post.e2e.js
@@ -3,13 +3,13 @@ const RedditListPostsPage = require('../pageobjects/reddit.list.posts.page');
 const RedditPostPage = require('../pageobjects/reddit.post.page');
 
 describe('Reddit posts page', () => {
-  // it('Change post list view', () => {
-  //   RedditListPostsPage.open();
+  it('Change post list view', () => {
+    RedditListPostsPage.open();
 
-  //   RedditListPostsPage.topPageCheckTitle();
-  //   RedditListPostsPage.topPageCheckDate();
-  //   RedditListPostsPage.topPageCommentsCount();
-  // });
+    RedditListPostsPage.topPageCheckTitle();
+    RedditListPostsPage.topPageCheckDate();
+    RedditListPostsPage.topPageCommentsCount();
+  });
 
   it('Post page actions', () => {
     RedditPostPage.open();

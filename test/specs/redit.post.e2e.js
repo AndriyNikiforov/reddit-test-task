dd682d867c6c480627b14c31387917216b8597a4
Андрей Никифоров <anikiforov@web100-khm.local>
2021-04-27 08:18:14 +0300

    Fix test.

--- test/specs/redit.post.e2e.js
+++ test/specs/redit.post.e2e.js
@@ -3,20 +3,20 @@ const RedditListPostsPage = require('../pageobjects/reddit.list.posts.page');
 const RedditPostPage = require('../pageobjects/reddit.post.page');
 
 describe('Reddit posts page', () => {
-  // it('Top posts check title', () => {
-  //   RedditListPostsPage.open();
-  //   RedditListPostsPage.topPageCheckTitle();
-  // });
+  it('Top posts check title', () => {
+    RedditListPostsPage.open();
+    RedditListPostsPage.topPageCheckTitle();
+  });
 
-  // it('Top posts check date', () => {
-  //   RedditListPostsPage.open();
-  //   RedditListPostsPage.topPageCheckDate();
-  // });
+  it('Top posts check date', () => {
+    RedditListPostsPage.open();
+    RedditListPostsPage.topPageCheckDate();
+  });
 
-  // it('Top posts check comments count', () => {
-  //   RedditListPostsPage.open();
-  //   RedditListPostsPage.topPageCommentsCount();
-  // });
+  it('Top posts check comments count', () => {
+    RedditListPostsPage.open();
+    RedditListPostsPage.topPageCommentsCount();
+  });
 
   it('Check new comments', () => {
     RedditPostPage.open();
@@ -30,12 +30,12 @@ describe('Reddit posts page', () => {
     RedditPostPage.checkTopComments();
   });
 
-  // it('Replay action', () => {
-  //   RedditPostPage.open();
-  //   RedditPostPage.openFirstPost();
-  //   RedditPostPage.openReplayModalWindow();
+  it('Replay action', () => {
+    RedditPostPage.open();
+    RedditPostPage.openFirstPost();
+    RedditPostPage.openReplayModalWindow();
 
-  //   expect(RedditPostPage.googleButton).toBeClickable();
-  //   expect(RedditPostPage.appleButton).toBeClickable();
-  // });
+    expect(RedditPostPage.googleButton).toBeClickable();
+    expect(RedditPostPage.appleButton).toBeClickable();
+  });
 });
